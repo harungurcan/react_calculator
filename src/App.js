c6dab7b20d75d1da59c94a61d220dcb9a12b3409
harungurcan <harungurcan@yahoo.com>
2023-07-13 18:16:27 +0000

    working tree is reset

--- src/App.js
+++ src/App.js
@@ -1,77 +1,12 @@
-import {
-  useState,
-  useRef
-} from "react"; 
+import react from "react";
 import "./App.css";
+import Functions from "./components/function";
 
-function App() { 
-  const inputRef = useRef(null); 
-  const resultRef = useRef(null); 
-  const [result, setResult] = useState(0); 
- 
-  //plus function
-  function plus(e) { 
-    e.preventDefault(); 
-    setResult((result) => result + Number(inputRef.current.value)); 
-  }; 
- 
-  //minus function
-  function minus(e) { 
-  	e.preventDefault(); 
-    setResult((result) => result - Number(inputRef.current.value)); 
-  }; 
-  
- 
-  //times function
-  function times(e) { 
-    e.preventDefault(); 
-    setResult((result) => result * Number(inputRef.current.value)); 
-  }; 
+const App = () => {
 
- 
-  //division function
-  function divide(e) { 
-    e.preventDefault(); 
-    setResult((result) => result / Number(inputRef.current.value)); 
-  }; 
- 
-  //resetting input text
-  function resetInput(e) { 
-    e.preventDefault();
-    inputRef.current.value = 0;  
-  }; 
- 
-  //resetting the result
-  function resetResult(e) { 
-  	e.preventDefault();
-    setResult((result) => result * 0 );
-    inputRef.current.value = 0; 
-  }; 
- 
-  return ( 
-    <div className="App"> 
-      <div> 
-        <h1>Simplest Working Calculator</h1> 
-      </div> 
-      <form> 
-        <p ref={resultRef}> 
-          {result} 
-        </p>
-        <input
-          pattern="[0-9]" 
-          ref={inputRef} 
-          type="number" 
-          placeholder="Type a number" 
-        /> 
-        <button onClick={plus}>add</button>
-        <button onClick={minus}>subtract</button>
-        <button onClick={times}>multiply</button>
-        <button onClick={divide}>divide</button>
-        <button onClick={resetInput}>reset input</button>
-        <button onClick={resetResult}>reset result</button> 
-      </form> 
-    </div> 
-  ); 
+  return (
+    <Functions />
+  );
 
-}
+};
 export default App; 
---
+++ src/components/function/Function.js
@@ -0,0 +1,77 @@
+import react from "react";
+import {
+    useState,
+    useRef
+  } from "react"; 
+
+const Functions = () => {
+    const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  //plus function
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value));
+  }; 
+ 
+  //minus function
+  function minus(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result - Number(inputRef.current.value)); 
+  }; 
+  
+ 
+  //times function
+  function times(e) { 
+    e.preventDefault(); 
+    setResult((result) => result * Number(inputRef.current.value)); 
+  }; 
+
+ 
+  //division function
+  function divide(e) { 
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputRef.current.value)); 
+  }; 
+ 
+  //resetting input text
+  function resetInput(e) { 
+    e.preventDefault();
+    inputRef.current.value = 0;  
+  }; 
+ 
+  //resetting the result
+  function resetResult(e) { 
+  	e.preventDefault();
+    setResult((result) => result * 0 );
+    inputRef.current.value = 0; 
+  }; 
+    return (
+        <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+      <p ref={resultRef}> 
+          {result} 
+      </p>
+      <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button> 
+      </form>
+
+    </div>
+    );
+}
+
+export default Functions;
