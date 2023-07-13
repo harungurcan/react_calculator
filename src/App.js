63a13fde94bc8722043783e74f1637a2aeb5cc88
harungurcan <harungurcan@yahoo.com>
2023-07-13 18:43:50 +0000

    Adding Header and updating working tree

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import react from "react";
+import React from "react";
 import "./App.css";
-import Functions from "./components/function";
+import Functions from "./components/function/Function";
+import Header from "./components/header/Header";
 
 const App = () => {
 
   return (
-    <Functions />
+    <>
+      <Header />
+      <Functions />
+    </>
   );
 
 };
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <p>A paragraph</p>
+        </div>
+    )
+
+};
+
+export default Header;
